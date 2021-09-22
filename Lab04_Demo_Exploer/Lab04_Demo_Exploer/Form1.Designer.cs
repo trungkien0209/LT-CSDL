a11e3f25f650b7b8d9461366ad366f1ec248b923
trungkien0209 <1911160@dlu.edu.vn>
2021-09-22 16:19:47 +0700

    LT-CSDL

---
+++ Lab04_Demo_Exploer/Lab04_Demo_Exploer/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿
+namespace Lab04_Demo_Exploer
+{
+    partial class frmExplorer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmExplorer));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.treeViewFolder = new System.Windows.Forms.TreeView();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.imgSmall = new System.Windows.Forms.ImageList(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.copyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pasteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // treeViewFolder
+            // 
+            this.treeViewFolder.ImageIndex = 0;
+            this.treeViewFolder.ImageList = this.imgSmall;
+            this.treeViewFolder.Location = new System.Drawing.Point(12, 27);
+            this.treeViewFolder.Name = "treeViewFolder";
+            this.treeViewFolder.SelectedImageIndex = 0;
+            this.treeViewFolder.Size = new System.Drawing.Size(202, 361);
+            this.treeViewFolder.TabIndex = 1;
+            this.treeViewFolder.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeViewFolder_AfterSelect);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.ContextMenuStrip = this.contextMenuStrip1;
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(220, 27);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(568, 361);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // imgSmall
+            // 
+            this.imgSmall.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imgSmall.ImageStream")));
+            this.imgSmall.TransparentColor = System.Drawing.Color.Transparent;
+            this.imgSmall.Images.SetKeyName(0, "Desktop");
+            this.imgSmall.Images.SetKeyName(1, "MyComputer");
+            this.imgSmall.Images.SetKeyName(2, "MyDocuments");
+            this.imgSmall.Images.SetKeyName(3, "HDD");
+            this.imgSmall.Images.SetKeyName(4, "CD");
+            this.imgSmall.Images.SetKeyName(5, "Removable");
+            this.imgSmall.Images.SetKeyName(6, "Folder");
+            this.imgSmall.Images.SetKeyName(7, "Shortcut");
+            this.imgSmall.Images.SetKeyName(8, "File");
+            this.imgSmall.Images.SetKeyName(9, "Network");
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.copyToolStripMenuItem,
+            this.cutToolStripMenuItem,
+            this.pasteToolStripMenuItem,
+            this.deleteToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 136);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "File Name";
+            this.columnHeader1.Width = 290;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Date Modified";
+            this.columnHeader2.Width = 113;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Type";
+            this.columnHeader3.Width = 78;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Size";
+            this.columnHeader4.Width = 78;
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // copyToolStripMenuItem
+            // 
+            this.copyToolStripMenuItem.Name = "copyToolStripMenuItem";
+            this.copyToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.copyToolStripMenuItem.Text = "Copy";
+            this.copyToolStripMenuItem.Click += new System.EventHandler(this.copyToolStripMenuItem_Click);
+            // 
+            // cutToolStripMenuItem
+            // 
+            this.cutToolStripMenuItem.Name = "cutToolStripMenuItem";
+            this.cutToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.cutToolStripMenuItem.Text = "Cut";
+            this.cutToolStripMenuItem.Click += new System.EventHandler(this.cutToolStripMenuItem_Click);
+            // 
+            // pasteToolStripMenuItem
+            // 
+            this.pasteToolStripMenuItem.Name = "pasteToolStripMenuItem";
+            this.pasteToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.pasteToolStripMenuItem.Text = "Paste";
+            this.pasteToolStripMenuItem.Click += new System.EventHandler(this.pasteToolStripMenuItem_Click);
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.deleteToolStripMenuItem.Text = "Delete";
+            this.deleteToolStripMenuItem.Click += new System.EventHandler(this.deleteToolStripMenuItem_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 390);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // frmExplorer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 412);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.treeViewFolder);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmExplorer";
+            this.Text = "Demo Explorer";
+            this.Load += new System.EventHandler(this.frmExplorer_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.TreeView treeViewFolder;
+        private System.Windows.Forms.ImageList imgSmall;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem copyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pasteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ Lab04_Demo_Exploer/Lab04_Demo_Exploer/Form1.cs
@@ -0,0 +1,173 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab04_Demo_Exploer
+{
+    public partial class frmExplorer : Form
+    {
+        public frmExplorer()
+        {
+            InitializeComponent();
+        }
+
+        private string ImageKeyDriver(DriveInfo drive)
+        {
+            string type = "";
+            switch (drive.DriveType)
+            {
+                case DriveType.Fixed:
+                    type = "HDD";
+                    break;
+                case DriveType.CDRom:
+                    type = "CD";
+                    break;
+                case DriveType.Removable:
+                    type = "Removable";
+                    break;
+                case DriveType.Network:
+                    type = "Network";
+                    break;
+                default:
+                    type = "HDD";
+                    break;
+            }
+            return type;
+        }
+        //phương thức load các ổ đĩa
+        private void LoadDrive(TreeNode tn)
+        {
+            TreeNode tnode = new TreeNode();
+
+            foreach (DriveInfo dr in DriveInfo.GetDrives())
+            {
+                tnode = new TreeNode();
+                tnode.Text = dr.Name;
+                tnode.Tag = dr.ToString();
+                tnode.ImageKey = ImageKeyDriver(dr);
+                tnode.ImageKey = ImageKeyDriver(dr);
+                tn.Nodes.Add(tnode);
+            }
+        }
+        //Phương thức thêm thư mục vào tnParent
+        private void InsertFolder(TreeNode tnParent)
+        {
+            try
+            {
+                DirectoryInfo dir = new DirectoryInfo(tnParent.Tag.ToString());
+                foreach (DirectoryInfo dircur in dir.GetDirectories())
+                {
+                    TreeNode tnChild = new TreeNode(dircur.Name, 6, 6);
+                    tnChild.Tag = dircur.FullName;
+                    tnParent.Nodes.Add(tnChild);
+                }
+            }
+            catch
+            { }
+        }
+        //Phương thức file vào listviewFile của tnParent
+        private void InserFile(TreeNode tnParent)
+        {
+            try
+            {
+                DirectoryInfo dir = new DirectoryInfo(tnParent.Tag.ToString());
+                this.listView1.Items.Clear();
+                foreach (FileInfo filecur in dir.GetFiles())
+                {
+                    ListViewItem lvitem = new ListViewItem(filecur.Name);
+                    lvitem.SubItems.Add(filecur.LastWriteTime.ToShortDateString());
+                    lvitem.SubItems.Add(filecur.Extension);
+                    lvitem.SubItems.Add((filecur.Length / 1024).ToString());
+                    this.listView1.Items.Add(lvitem);
+                }
+                this.toolStripStatusLabel1.Text = "Tổng số Files: " +
+              this.listView1.Items.Count;
+            }
+            catch
+            { }
+        }
+        //Phương thức thêm đĩa hoặc thư mục và file của node tnparent
+        private void InsertChildParent(TreeNode tnparent)
+        {
+            if (tnparent != null)
+                if (tnparent.Level == 0)
+                {
+                    TreeNode tnMyDocuments = new TreeNode("My Documents", 2, 2);
+                    tnMyDocuments.Tag = SpecialDirectories.MyDocuments;
+                    TreeNode tnMyComputer = new TreeNode("My Computer", 1, 1);
+                    tnMyComputer.Tag = "My Computer";
+                    tnparent.Nodes.Insert(0, tnMyDocuments);
+                    tnparent.Nodes.Insert(1, tnMyComputer);
+                }
+                else
+                if (tnparent.Nodes.Count == 0)
+                {
+                    if (tnparent.Text == "My Computer")
+                    {
+                        LoadDrive(tnparent);
+                        return;
+                    }
+                    InsertFolder(tnparent);
+                }
+            InserFile(tnparent);
+        }
+
+        private void frmExplorer_Load(object sender, EventArgs e)
+        {
+            TreeNode tnode = new TreeNode();
+            tnode.Text = "Desktop";
+            tnode.Tag = SpecialDirectories.Desktop;
+            tnode.ImageKey = "Desktop";
+            tnode.SelectedImageKey = "Desktop";
+            int root = this.treeViewFolder.Nodes.Add(tnode);
+            string name = Application.StartupPath;
+            name = name.Substring(0, name.LastIndexOf('\\'));
+            name = name.Substring(0, name.LastIndexOf('\\'));
+        }
+
+        private void treeViewFolder_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            TreeNode tnParent = e.Node;
+            //tnCurrent = e.Node;
+            InsertChildParent(tnParent);
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
